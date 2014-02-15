f0f6eca3b1e86eb342d236ae9232fba2df931cf8
Brian Kim <byk.briankim@gmail.com>
2014-02-15 14:39:30 -0800

    Adding basic pebble app files to repo

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "pebbrawl",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x7A, 0x2D, 0xAF, 0x5E, 0x62, 0x1D, 0x45, 0x43, 0xB5, 0x1C, 0xD0, 0xBB, 0x95, 0xC6, 0x76, 0x99 },
+  .virtual_size = 0xb6b6
+};
